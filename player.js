f33f85416966cdb7cba5602d86293affa3a521d8
rificata <ricfil2508@gmail.com>
2026-01-29 23:54:51 +0000

    Add files via upload

--- player.js
+++ player.js
@@ -3,7 +3,45 @@ window.onload = () => {
 
     // Carregar o primeiro canal automaticamente
     if (channels.length > 0) {
-        player.src = channels[0].url;
-        player.play();
+        if (channels[0].type === "youtube") {
+            loadYouTube(channels[0].url);
+        } else {
+            player.src = channels[0].url;
+            player.play();
+        }
     }
+
+    const list = document.getElementById("channel-list");
+
+    channels.forEach(ch => {
+        const div = document.createElement("div");
+        div.className = "channel";
+        div.dataset.name = ch.name;
+
+        div.innerHTML = `<img src="${ch.logo}" alt="${ch.name}">`;
+
+        div.onclick = () => {
+            if (ch.type === "youtube") {
+                loadYouTube(ch.url);
+            } else {
+                unloadYouTube();
+                player.src = ch.url;
+                player.play();
+            }
+        };
+
+        list.appendChild(div);
+    });
 };
+
+// Função para carregar YouTube no player
+function loadYouTube(embedUrl) {
+    const container = document.getElementById("player-area");
+    container.innerHTML = `<iframe id="yt-frame" src="${embedUrl}" frameborder="0" allowfullscreen></iframe>`;
+}
+
+// Voltar ao player normal
+function unloadYouTube() {
+    const container = document.getElementById("player-area");
+    container.innerHTML = `<video id="tv-player" controls autoplay></video>`;
+}
